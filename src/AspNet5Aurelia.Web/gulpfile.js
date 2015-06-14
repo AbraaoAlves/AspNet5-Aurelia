f6fc5b9e689272e44509f31a72bb990cb8253fe1
danilojrr <danilobjr@gmail.com>
2015-06-14 13:10:46 -0300

    gulp-sass task using sourcemaps

--- src/AspNet5Aurelia.Web/gulpfile.js
+++ src/AspNet5Aurelia.Web/gulpfile.js
@@ -1,14 +1,18 @@
 ﻿/// <binding Clean='clean' />
 
 var gulp = require("gulp"),
-  rimraf = require("rimraf"),
-  fs = require("fs");
+    rimraf = require("rimraf"),
+    fs = require("fs"),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass');
 
 eval("var project = " + fs.readFileSync("./project.json"));
 
 var paths = {
-  bower: "./bower_components/",
-  lib: "./" + project.webroot + "/lib/"
+    bower: "./bower_components/",
+    lib: "./" + project.webroot + "/lib/",
+    cssFolder: './' + project.webroot + '/css',
+    sassFiles: './Assets/Styles/**/*.scss'
 };
 
 gulp.task("clean", function (cb) {
@@ -30,3 +34,17 @@ gulp.task("copy", ["clean"], function () {
       .pipe(gulp.dest(paths.lib + destinationDir));
   }
 });
+
+gulp.task('sass', function () {
+    gulp.src(paths.sassFiles)
+        .pipe(sourcemaps.init())
+            .pipe(sass().on('errer', sass.logError))
+        .pipe(sourcemaps.write('/maps'))
+        .pipe(gulp.dest(paths.cssFolder));
+});
+
+gulp.task('watch', ['sass'], function () {
+    gulp.watch(paths.sassFiles, ['sass']);
+});
+
+gulp.task('default', ['watch']);
