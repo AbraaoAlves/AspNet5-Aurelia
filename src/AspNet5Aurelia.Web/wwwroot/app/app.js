3c8970f6448e10ca69975a9954806694593c600f
danilojrr <danilobjr@gmail.com>
2015-06-13 22:22:33 -0300

    uploading source code with: aspnet5 mvc template, aurelia, jspm and materialize

---
+++ src/AspNet5Aurelia.Web/wwwroot/app/app.js
@@ -0,0 +1,14 @@
+﻿import 'materialize';
+import 'materialize/dist/js/materialize';
+
+export class App {
+	configureRouter(config, router) {
+		config.title = 'AspNet 5 Aurelia';
+		
+		config.map([
+			{ route: ['', 'login'], moduleId: './views/account/login' }
+		]);
+		
+		this.router = router;
+	}
+}
---
+++ src/AspNet5Aurelia.Web/wwwroot/app/views/account/login.js
@@ -0,0 +1,5 @@
+export class Login {
+	login() {
+		console.log('login');
+	}
+}
